5d4b56ded7c171a0e834c0561544f54fbf4bd17b
cristian.ataliba <cristian.ataliba@outlook.com>
2025-07-26 16:30:29 -0300

    Inclui 3 cenários de Teste: LoginSucesso, EmailInvalido, SenhaInvalida

--- webdojo/web/cypress/e2e/login.cy.js
+++ webdojo/web/cypress/e2e/login.cy.js
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io') 
-  })
-})
+describe('Login', () => {
+  it('Deve logar com sucesso', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+    cy.get('input[placeholder = "Digite seu e-mail"]').type('papito@webdojo.com')
+    cy.get('input[placeholder = "Digite sua senha"]').type('katana123')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Fernando Papito').should('be.visible')
+  });
+
+  it('Não deve logar com senha inválida', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+    cy.get('input[placeholder = "Digite seu e-mail"]').type('papito@webdojo.com')
+    cy.get('input[placeholder = "Digite sua senha"]').type('katana124')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Acesso negado! Tente novamente.').should('be.visible')
+  });
+
+  it('Não deve logar com E-mail inválido', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+    cy.get('input[placeholder = "Digite seu e-mail"]').type('papito@webdojoo.com')
+    cy.get('input[placeholder = "Digite sua senha"]').type('katana123')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Acesso negado! Tente novamente.').should('be.visible')
+  });
+});
