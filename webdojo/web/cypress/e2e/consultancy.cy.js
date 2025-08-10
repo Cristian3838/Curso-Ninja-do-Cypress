6473015d2b0d454c55bd747ac82d054c2f22dcbf
cristian.ataliba <cristian.ataliba@outlook.com>
2025-08-10 13:00:49 -0300

    Treinamento Ninja do Cypress Config - Custom Commands

--- webdojo/web/cypress/e2e/consultancy.cy.js
+++ webdojo/web/cypress/e2e/consultancy.cy.js
@@ -3,11 +3,5 @@ describe('Formulário de Consultoria', () => {
         cy.start()
         cy.submitLoginForm('papito@webdojo.com', 'katana123')
         cy.goTo('Formulários', 'Consultoria')
-      
     });
 });
-
-Cypress.Commands.add('goTo', (buttonName, pageTitle)=>{
-    cy.contains('button', buttonName).should('be.visible').click()
-    cy.contains('h1', pageTitle).should('be.visible')
-})
